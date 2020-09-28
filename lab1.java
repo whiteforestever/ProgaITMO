f988b8666a038feb36434f0521ae12b9d8acb5cf
whiteforestever <72013920+whiteforestever@users.noreply.github.com>
2020-09-28 14:12:16 +0300

    Update lab1.java
    maybe complete

--- lab1.java
+++ lab1.java
@@ -5,37 +5,37 @@ public class proba{
 int i;
 final int a = -7, b = 3;
 int j = 18;
-double[] x = new double[11];
 long[] d = new long[8];
+double[] x = new double[11];
 double[][] v = new double[8][11];
 
-for(i = 0; i < 8; ++i){
-		d[i] = j;
-		j-= 2;
+for(i = 0; i < 8; ++i){	
+	d[i] = j;
+	j-= 2;
 }
 
 for(i = 0; i < 11; ++i){
-		x[i] = ((Math.random() * (b-a)) + a);
+	x[i] = ((Math.random() * (b-a)) + a);
 }
 
 for(i = 0; i  <8; ++i){
-		for(j = 0; j < 11; ++j){
-			if(d[i] == 12){
-v[i][j] = Math.atan(Math.pow (Math.E, -(Math.abs (x[i]))));
-			}
-			else if(d[i] == 6 || d[i] == 10  || d[i] == 16 || d[i] == 18){
-v[i][j] = Math.asin(Math.sin(Math.asin( (x[i]-2)/1*Math.E+1)));
-			}
-			else{
-v[i][j] = Math.tan( Math.tan(Math.pow(Math.tan(x[i]), 1.0/3)));
-			}
+	for(j = 0; j < 11; ++j){
+		if(d[i] == 12){
+			v[i][j] = Math.atan(Math.pow(Math.E, -(Math.abs (x[i])))) - 1;
 		}
+		else if(d[i] == 6 || d[i] == 10  || d[i] == 16 || d[i] == 18){
+			v[i][j] = Math.asin(Math.sin(Math.asin( (x[i]-2)/1*Math.E+1) ));
+		}
+		else{
+			v[i][j] = Math.tan( Math.tan(Math.pow(Math.tan(x[i]), 1.0/3)));
+		}
+	}
 }
 
-for(i = 0; i  <8; ++i){
-		for(j = 0; j < 11; ++j){
-			System.out.printf("%.4f\t", v[i][j]);
-		}
+for(i = 0; i < 8; ++i){
+	for(j = 0; j < 11; ++j){
+		System.out.printf("%.4f\t", v[i][j]);
+	}
 System.out.print("\n");
 }
 
